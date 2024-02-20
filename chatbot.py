a03e57b55f2aea30eea92d52fda1f9ef2ab6eacb
biniyam69 <biniyam.ajaw@astu.edu.et>
2024-02-20 15:30:24 +0000

    RAG with chroma

--- chatbot.py
+++ chatbot.py
@@ -6,21 +6,65 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 from trulens_eval import TruChain, Feedback, OpenAI, Huggingface, Tru
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import UnstructuredHTMLLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings.sentence_transformer import (
+    SentenceTransformerEmbeddings,
+)
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
+
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
 hugs = Huggingface()
 openai = OpenAI()
 tru = Tru()
 
-template = """You are a chatbot having a conversation with a human.
-        {chat_history}
-        Human: {human_input}
-        Chatbot:"""
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+
+### Load Docs ###
+
+loader = DirectoryLoader(path='/home/biniyam/TenAcademy/AI-Contract-Lawyer/notebook/imdb_data/', loader_cls=UnstructuredHTMLLoader)
+docs = loader.load()
+
+text_spliter = CharacterTextSplitter(chunk_size=512, chunk_overlap=0)
+documents = text_spliter.split_documents(docs)
+
+embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+
+vectorstore = Chroma(docs, embedding_function=embedding_function)
+
+retriever = vectorstore.as_retriever()
+
+
+
+
+
+##### Chain #####
+
+template = """You will answer the following questions based on the following context:
+{context}
+
+Question: {question}"""
+
+
     
-prompt = PromptTemplate(input_variables=["chat_history", "human_input"], template=template)
+prompt = PromptTemplate(input_variables=["context", "question"], template=template)
 
 memory = ConversationBufferMemory(memory_key="chat_history", max_memory_length=10)
 llm = ChatOpenAI(model_name="gpt-3.5-turbo")
-chain = LLMChain(llm=llm, prompt=prompt, memory=memory, verbose=True)
+chain = (RunnableParallel({"context": retriever, "question": RunnablePassthrough()})
+                        | prompt
+                        | llm
+                        | StrOutputParser())
+
+##### Evaluation #####
 
 f_relevance = Feedback(openai.relevance).on_input_output()
 
@@ -34,6 +78,9 @@ chain_recorder = TruChain(
     chain, app_id='Simple RAG', feedbacks=[f_relevance, f_hate, f_violent, f_selfharmed, f_malice]
     
 )
+
+
+
 
 # Streamlit frontend
 st.title("Contextual Chatbot")
